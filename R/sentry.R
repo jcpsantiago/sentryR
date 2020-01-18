0c749c97fc56198dc4307c48d9445445372893d8
João Santiago <joaocpsantiago@gmail.com>
2020-01-18 15:58:06 +0100

    start majro refactoring
    Uses purer functions to make tests easier;
    Follows the Sentry SDK recommendations closer;

--- R/sentry.R
+++ R/sentry.R
@@ -1,39 +1,245 @@
-#' Parse Sentry DSN
+#' Parse a Sentry DSN into its components
 #'
-#' @param dsn the DSN of a Sentry project as a character string.
+#' @param dsn the DSN of a Sentry project.
 #'
-#' @return populates the .SentryEnv environment with character strings
-#'
-#' @importFrom stringr str_match regex
-#' @importFrom stats setNames
-#'
+#' @return a named list with parsed elements of the DSN
 #' @export
-sentry.config <- function(dsn) {
-  l <- stats::setNames(
+#'
+#' @examples
+#' parse_dsn("https://1234@sentry.io/1")
+parse_dsn <- function(dsn) {
+  if (!is.character(dsn)) {
+    stop(
+      paste("dsn must be a character string, not", class(dsn))
+    )
+  }
+
+  dsn_fields <- stats::setNames(
     as.list(stringr::str_match(dsn, stringr::regex("(.*)://(\\w*)(:(\\w*))?@(.*)/(.*)"))),
     c("dsn", "protocol", "public_key", "ignore", "secret_key", "host", "project_id")
   )
 
-  # Was sentry correctly configured? Abort if a dsn is passed (dsn != "") AND no dsn could be parsed
-  # by the regex (is.na(l$dsn)) – the following line is the inverse statement as per de Morgan's law
-  stopifnot(dsn == "" || !is.na(l$dsn))
+  if (is.na(dsn_fields$dsn)) {
+    stop("Invalid DSN! Expected format is 'https://<public_key>@<host>/<project_id>' ",
+         "but received '", dsn, "' instead.")
+  }
 
-  invisible(list2env(l, .SentryEnv))
+  mandatory_fields_present <- sapply(
+    c("public_key", "host", "project_id"),
+    function(x) !is.na(dsn_fields[[x]]) & dsn_fields[[x]] != "")
+
+  if (!all(mandatory_fields_present)) {
+    stop("Expected fields 'https://<public_key>@<host>/<project_id>', but can't find ",
+             paste(names(mandatory_fields_present)[!mandatory_fields_present],
+                   collapse = ", "), " in '", dsn, "'. Please check your DSN.")
+  }
+
+  return(dsn_fields)
+}
+
+
+#' Configure Sentry
+#'
+#' @param dsn the DSN of a Sentry project.
+#'
+#' @return populates the .SentryEnv environment with character strings
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' configure_sentry("https://12345abcddbc45e49773bb1ca8d9c533@sentry.io/1234567")
+#' .SentryEnv$host # sentry.io
+#' }
+configure_sentry <- function(dsn) {
+
+  if (length(dsn) > 1) {
+    stop("Expected one dsn, but received ", length(dsn), " instead.")
+  }
+
+  parsed <- parse_dsn(dsn)
+
+  invisible(list2env(parsed, .SentryEnv))
 }
 
 
 #' Check if Sentry is configured
 #'
 #' @return boolean
-#' @importFrom purrr map_lgl
-sentry.configured <- function() {
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' configure_sentry("https://12345abcddbc45e49773bb1ca8d9c533@sentry.io/1234567")
+#' is_sentry_configured() # TRUE
+#' }
+is_sentry_configured <- function() {
 
-  mandatory_fields <- purrr::map_lgl(c("public_key", "host", "project_id"),
-                                     ~exists(., envir = .SentryEnv))
+  mandatory_fields <- sapply(
+    c("public_key", "host", "project_id"),
+    function(x) exists(x, envir = .SentryEnv))
 
-  all(mandatory_fields) && all(!is.na(c(.SentryEnv$public_key,
-                                    .SentryEnv$host,
-                                    .SentryEnv$project_id)))
+  all_fields_present <- all(mandatory_fields) && all(!is.na(c(
+                          .SentryEnv$public_key,
+                          .SentryEnv$host,
+                          .SentryEnv$project_id
+                        )))
+
+  if (!all_fields_present) {
+    message(
+      paste0("Expected public_key, host and project_id to be present but can't find ",
+             paste(names(mandatory_fields)[!mandatory_fields],
+                   collapse = ", "), "."))
+
+    return(FALSE)
+  }
+
+  return(all_fields_present)
+}
+
+
+#' Prepare JSON payload for Sentry
+#'
+#' @param ... named parameters
+#'
+#' @return
+#' @export
+#'
+#' @examples
+prepare_payload <- function(...) {
+  # FIXME: don't allow unnamed lists e.g. prepare_paylaod(list(foo = 12, bar = 45))
+  if (any(names(c(...)) == "")) {
+    stop("All elements must be named!")
+  }
+
+  user_inputs <- list(...)
+
+  # TODO: check that user_inputs contains only valid names
+  # according to Sentry's documentation, and NULLify those not overwriteable
+
+  # Hexadecimal string representing a uuid4 value.
+  # The length is exactly 32 characters. Dashes are not allowed.
+  uuid <- gsub(pattern = "-", replacement = "",
+               x = uuid::UUIDgenerate(use.time = TRUE))
+
+  defaults <- list(
+    # Sentry will treat the timezone as UTC/GMT by default
+    timestamp = strftime(as.POSIXlt(Sys.time(), tz = "GMT"), "%Y-%m-%dT%H:%M:%SZ"),
+    logger = dplyr::if_else(is.null(user_inputs$logger), "R", user_inputs$logger),
+    platform = "R",
+    sdk = list(
+      name = "SentryR",
+      version = as.character(packageVersion("SentryR"))),
+    event_id = uuid
+  )
+
+  if ("logger" %in% names(user_inputs)) {
+    user_inputs$logger <- NULL
+  }
+
+  defaults_plus_userfields <- append(defaults, user_inputs)
+
+  payload <- jsonlite::toJSON(defaults_plus_userfields,
+                              auto_unbox = TRUE,
+                              null = 'null')
+
+  return(payload)
+}
+
+
+#' Send a message to a Sentry server
+#'
+#' @param ... named parameters
+#'
+#' @return sends message to Sentry
+#' @export
+#'
+#' @examples
+capture <- function(...) {
+
+  if (!is_sentry_configured()) {
+    stop("Sentry is not configured!")
+  }
+
+  payload <- prepare_payload(...)
+
+  resp <- httr::POST(
+    url = sentry_url(),
+    body = payload, encode = "json",
+    httr::add_headers(.headers = sentry_headers())
+  )
+
+  resp_status_code <- httr::status_code(resp)
+
+  if (resp_status_code == 201 || resp_status_code == 200) {
+    resp_body <- httr::content(resp)
+    message("Your event was recorder in Sentry with ID ", resp_body$id)
+  } else {
+    warning(
+      "Error connecting to Sentry:",
+      httr::content(resp, "text", encoding = "UTF-8")
+    )
+  }
+}
+
+
+#' Report a message
+#'
+#' @param .message message text
+#' @param ... optional additional named paramters
+#' @param .level the level of the message. Default: "info"
+#'
+#' @return nothing; sends message to Sentry
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' capture_message("this is an important message", logger = "my.logger")
+#' }
+capture_message <- function(.message, ..., .level = "info") {
+  # TODO: hello? happy path?
+  capture(message = .message, ..., level = .level)
+}
+
+
+#' Report an error or exception object
+#'
+#' @param error an error object
+#' @param ... optional additional named paramters
+#' @param .level the level of the message. Default: "error"
+#'
+#' @return nothing; sends error to Sentry
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' capture_exception(simpleError("foo"), tags = list(version = "1.0"))
+#' }
+capture_exception <- function(error, ..., .level = "error") {
+  # TODO: hello? happy path?
+  # TODO: wrangling the error should be a pure function prepare_error()
+
+  if (!is.null(error$calls)) {
+    stacktrace <- calls2stacktrace(error$calls)
+  } else {
+    stacktrace <- data.frame()
+  }
+
+  error_type <- paste(class(error), collapse = ",")
+  error_message <- gsub('(\\n|\\")', "", as.character(error))
+
+  exception_payload <- list(
+    exception = tibble::tibble(
+      type = error_type,
+      message = error_message,
+      stacktrace = list(
+        frames = stacktrace
+      )
+    )
+  )
+
+  capture(exception = exception_payload, ..., level = .level)
 }
 
 
@@ -51,7 +257,7 @@ sentry.configured <- function() {
 #' @importFrom httr POST add_headers status_code
 #' @export
 sentry.captureException <- function(error, req, rows_per_field = 10) {
-  if (!sentry.configured()) {
+  if (!is_sentry_configured()) {
     warning("An error occured but Sentry is not configured.")
     return()
   }
@@ -64,7 +270,7 @@ sentry.captureException <- function(error, req, rows_per_field = 10) {
 
   # Ensure a non-empty body for jsonlite::fromJSON(req$postBody)
   if (identical(req$postBody, character(0))) {
-    req$postBody <- '[]'
+    req$postBody <- "[]"
   }
 
   # Ensure that req$HTTP_CONTENT_TYPE is exists
@@ -124,26 +330,30 @@ sentry.captureException <- function(error, req, rows_per_field = 10) {
   if (httr::status_code(resp) == 201 || httr::status_code(resp) == 200) {
     message("Error successfully sent to Sentry, check your project for more details.\n")
   } else {
-    warning("Error connecting to Sentry:",
-            httr::content(resp, "text", encoding = "UTF-8"), "\n")
+    warning(
+      "Error connecting to Sentry:",
+      httr::content(resp, "text", encoding = "UTF-8"), "\n"
+    )
   }
 }
 
 
 #' Build the response URL
 #'
+#' @export
+#'
 #' @return a character string
-#' @importFrom glue glue
-.sentry.url <- function() {
+sentry_url <- function() {
   glue::glue("{protocol}://{host}/api/{project_id}/store/", .envir = .SentryEnv)
 }
 
 
 #' Set the response header
 #'
+#' @export
+#'
 #' @return a character vector
-#' @importFrom glue glue
-.sentry.header <- function() {
+sentry_headers <- function() {
   if (!is.na(.SentryEnv$secret_key)) {
     # looks nicer, but the \n could create some issues, so we remove them
     # just in case
@@ -152,14 +362,16 @@ sentry.captureException <- function(error, req, rows_per_field = 10) {
                                    sentry_timestamp={as.integer(Sys.time())},
                                    sentry_key={public_key},
                                    sentry_secret={secret_key}",
-                                   .envir = .SentryEnv) %>%
-        gsub("[\r\n]", "", .))
+      .envir = .SentryEnv
+    ) %>%
+      gsub("[\r\n]", "", .))
   } else {
     c("X-Sentry-Auth" = glue::glue("Sentry sentry_version=7,
                                    sentry_client=sentryR/{packageVersion('sentryR')},
                                    sentry_timestamp={as.integer(Sys.time())},
                                    sentry_key={public_key}",
-                                   .envir = .SentryEnv) %>%
-        gsub("[\r\n]", "", .))
+      .envir = .SentryEnv
+    ) %>%
+      gsub("[\r\n]", "", .))
   }
 }
--- tests/testthat/test-sentry.R
+++ tests/testthat/test-sentry.R
@@ -1,27 +1,72 @@
 context("test-sentry")
 
+test_that("parsing the dsn works", {
+  expect_error(parse_dsn(888))
+
+  test_dsn <- "https://1234@sentry.io"
+  expect_error(parse_dsn(test_dsn),
+               paste0("Invalid DSN! Expected format is 'https://<public_key>@<host>/<project_id>' but received ",
+                      "'https://1234@sentry\\.io' instead\\."))
+
+  test_dsn <- "https://1234@sentry.io/"
+  expect_error(parse_dsn(test_dsn),
+               paste0("Expected fields 'https://<public_key>@<host>/<project_id>', ",
+                      "but can't find project_id in 'https://1234@sentry\\.io/'",
+                      "\\. Please check your DSN\\."))
+
+  test_dsn <- "https://1234@sentry.io/1"
+  expect_equal(
+    names(parse_dsn(test_dsn)),
+    c("dsn", "protocol", "public_key", "ignore", "secret_key", "host", "project_id")
+  )
+  expect_equal(parse_dsn(test_dsn),
+               list(dsn = test_dsn, protocol = "https", public_key = "1234",
+                    ignore = NA_character_, secret_key = NA_character_,
+                    host = "sentry.io", project_id = "1")
+               )
+})
+
 test_that("setting configuration works", {
-  expect_error(sentry.config("http://www.purple.com"))
+  expect_error(configure_sentry(42))
+  expect_error(configure_sentry(c("https://1234@sentry.io/1", "https://1234@sentry.io/2")))
+})
+
+test_that("builds payload correctly", {
+  expect_error(prepare_payload(message = "foo", "bar"))
+
+  expect_match(
+    prepare_payload(message = "foo", level = "info"),
+    '\\{"timestamp":.*,"logger":"R","platform":"R","sdk":\\{"name":"SentryR","version":.*\\},"event_id":.*,"message":"foo","level":"info"\\}'
+  )
 })
 
 test_that("configuration is properly set", {
 
-  expect_false(sentry.configured())
+  expect_message(is_sentry_configured(),
+                 "Expected public_key, host and project_id to be present but can't find public_key, host, project_id\\.")
+  expect_false(is_sentry_configured())
 
   .SentryEnv$public_key <- "1234"
   .SentryEnv$host <- "sentry.io"
+
+  expect_message(is_sentry_configured(),
+                 "Expected public_key, host and project_id to be present but can't find project_id\\.")
+  expect_false(is_sentry_configured())
+
+
   .SentryEnv$project_id <- "1"
 
-  expect_true(sentry.configured())
+  expect_true(is_sentry_configured())
 
   rm(list = ls(envir = .SentryEnv), envir = .SentryEnv)
 
-  sentry.config("https://1234@sentry.io/1")
 
-  fields <- purrr::map_lgl(
+  configure_sentry("https://1234@sentry.io/1")
+
+  fields <- sapply(
     c("dsn", "protocol", "public_key", "ignore",
       "secret_key", "host", "project_id"),
-    ~exists(., envir = .SentryEnv))
+    function(x) exists(x, envir = .SentryEnv))
 
   expect_true(all(fields))
 
@@ -33,7 +78,7 @@ test_that("we build the correct response url", {
   .SentryEnv$host <- "sentry.io"
   .SentryEnv$project_id <- "1"
 
-  expect_equal(.sentry.url(), "https://sentry.io/api/1/store/")
+  expect_equal(sentry_url(), "https://sentry.io/api/1/store/")
 
   rm(list = ls(envir = .SentryEnv), envir = .SentryEnv)
 })
@@ -43,12 +88,12 @@ test_that("we build the correct headers", {
   .SentryEnv$public_key <- "1234"
   .SentryEnv$secret_key <- NA
 
-  expect_equal(.sentry.header(),
+  expect_equal(sentry_headers(),
                c("X-Sentry-Auth" = glue::glue("Sentry sentry_version=7,sentry_client=sentryR/{packageVersion('sentryR')},sentry_timestamp={as.integer(Sys.time())},sentry_key=1234")))
 
   # with the deprecated secret key
   .SentryEnv$secret_key <- "5678"
-  expect_equal(.sentry.header(),
+  expect_equal(sentry_headers(),
                c("X-Sentry-Auth" = glue::glue("Sentry sentry_version=7,sentry_client=sentryR/{packageVersion('sentryR')},sentry_timestamp={as.integer(Sys.time())},sentry_key=1234,sentry_secret=5678")))
 
   rm(list = ls(envir = .SentryEnv), envir = .SentryEnv)
@@ -57,7 +102,7 @@ test_that("we build the correct headers", {
 test_that("captureException complains", {
   source(test_path("mocks.R"))
 
-  with_mock(sentry.configured = not_configured, {
+  with_mock(is_sentry_configured = not_configured, {
     expect_warning(sentry.captureException(error_nocalls, req))
   }, .env = "sentryR")
 
@@ -67,7 +112,7 @@ test_that("captureException complains", {
 test_that("inform about Sentry responses", {
   source(test_path("mocks.R"))
 
-  mockery::stub(sentry.captureException, "sentry.configured", TRUE)
+  mockery::stub(sentry.captureException, "is_sentry_configured", TRUE)
   mockery::stub(sentry.captureException, "httr::POST", "foobar")
   mockery::stub(sentry.captureException, "httr::status_code", 200)
 
