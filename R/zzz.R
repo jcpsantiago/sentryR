6ded8f2e08c14736730b14641876cddd3d413cd8
João Santiago <joaocpsantiago@gmail.com>
2020-01-20 15:19:49 +0100

    pre build part of the payload on load

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,37 @@
 # sentryR uses an environment to track state within the package
+# pre-calculates a couple of re-used values
 # plays well with the glue package too
-.SentryEnv <- new.env()
+sentry_env <- new.env(parent = emptyenv())
+sentry_env$pkg_version <- utils::packageVersion('sentryR')
+sentry_env$as.integer <- base::as.integer
+sentry_env$Sys.time <- base::Sys.time
+
+installed_pkgs_df <- as.data.frame(installed.packages(),
+                                   stringsAsFactors = FALSE)
+
+versions <- installed_pkgs_df$Version
+names(versions) <- installed_pkgs_df$Package
+
+modules <- as.list(versions)
+
+sentry_env$modules <- modules
+
+sys_info <- Sys.info()
+
+context <- list(
+  os = list(
+    name = sys_info[["sysname"]],
+    version = sys_info[["release"]],
+    kernel_version = sys_info[["version"]]
+  ),
+  runtime = list(
+    version = glue::glue("{R.version$major}.{R.version$minor}"),
+    type = "runtime",
+    name = "R",
+    build = R.version$version.string
+  ))
+
+sentry_env$context <- context
 
 # shut up R CMD check
 globalVariables(".")
