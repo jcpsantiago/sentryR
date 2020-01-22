16ec3151a9bb8332feef17871e4f84ae85bfb88e
João Santiago <joaocpsantiago@gmail.com>
2020-01-22 22:06:26 +0100

    no need for .onload

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,4 @@
 # sentryR uses an environment to track state within the package
 # pre-calculates a couple of re-used values
 # plays well with the glue package too
-# TODO: do we really need to use emptyenv() here?
 .sentry_env <- new.env()
-
-pkg_version <- as.character(utils::packageVersion("SentryR"))
-
-.onLoad <- function(libname, pkgname) {
-  .sentry_env$pkg_version <- pkg_version
-  .sentry_env$as.integer <- base::as.integer
-  .sentry_env$Sys.time <- base::Sys.time
-}
