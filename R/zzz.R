8508e7dd0fa1842bfee5e40cac97da7aedea828a
João Santiago <joaocpsantiago@gmail.com>
2020-01-23 09:05:58 +0100

    hardcode version in zzz because of CRAN

--- R/zzz.R
+++ R/zzz.R
@@ -2,4 +2,7 @@
 # pre-calculates a couple of re-used values
 # plays well with the glue package too
 .sentry_env <- new.env()
-.sentry_env$pkg_version <- utils::packageVersion("SentryR")
+# FIXME: should use utils::packageVersion() instead, but that's not working
+# with CRAN because the package is checked before being installed, thus there is
+# no sentryR package to get the version from
+.sentry_env$pkg_version <- "1.0.0"
